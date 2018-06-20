bfce69276da0484c32550390fa547aae413a6cc9
akina42 <akina_ak@hotmail.com>
2018-06-19 21:38:33 -0300

    Add métodos no gerenciador e inclusão do atributo fila na classe conta

--- src/GerenciadorConcorrenciaServidorClienteImpl.java
+++ src/GerenciadorConcorrenciaServidorClienteImpl.java
@@ -10,43 +10,66 @@
  */
 
 import java.rmi.Naming;
-import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class GerenciadorConcorrenciaServidorClienteImpl extends java.rmi.server.UnicastRemoteObject implements IGerenciadorConcorrenciaServidorCliente{
     public GerenciadorConcorrenciaServidorClienteImpl()
             throws java.rmi.RemoteException {
         super();
     }
-    
-    public Boolean verificaBloqueioConta(int numConta){
+
+    public Boolean verificaBloqueioEscritaConta(int numConta){
         Boolean estaBloqueadoEscrita = true;
-        Boolean estaBloqueadoLeitura = true;
         try{
-            IRepositorioServer repositorio = 
+            IRepositorioServer repositorio =
                     (IRepositorioServer) Naming.lookup("//127.0.0.1:1099/RepositorioServer");
             Conta conta = repositorio.encontraConta(numConta);
             estaBloqueadoEscrita = conta.isBloqueadoEscrita();
-            estaBloqueadoLeitura = conta.isBloqueadoLeitura();            
         }
         catch(Exception e){
             System.out.println("Conta inexistente. Erro ao acessar: " + e);
         }
-        
-        //TODO em que momento vou diferenciar esse bloqueio? implementar outro método?
-        
-        if(estaBloqueadoEscrita || estaBloqueadoLeitura){
-            return true;
-        }
-        return false;
-    }
-    
-    
-    public void recebeDadosEntrada(int opcao){
-                
-    }
-        
-    
-    
-    
-}
 
+        return estaBloqueadoEscrita;
+    }
+
+    public Boolean verificaBloqueioLeituraConta(int numConta){
+        Boolean estaBloqueadoLeitura = true;
+        try{
+            IRepositorioServer repositorio =
+                    (IRepositorioServer) Naming.lookup("//127.0.0.1:1099/RepositorioServer");
+            Conta conta = repositorio.encontraConta(numConta);
+            estaBloqueadoLeitura = conta.isBloqueadoEscrita();
+        }
+        catch(Exception e){
+            System.out.println("Conta inexistente. Erro ao acessar: " + e);
+        }
+
+        return estaBloqueadoLeitura;
+    }
+
+
+    public void recebeDadosEntradaAlteracao(int opcao, int idCliente,
+            ArrayList<Integer> contasSelecionadas, float valor){
+
+        contasSelecionadas.stream().forEach(conta ->{
+            System.out.println("Teste");
+        });
+
+    }
+
+
+    public void recebeDadosEntradaConsulta(int opcao, int idCliente,
+            ArrayList<Integer> contasSelecionadas){
+
+        contasSelecionadas.stream().forEach(conta ->{
+            System.out.println("Teste");
+        });
+
+    }
+
+
+
+
+}
